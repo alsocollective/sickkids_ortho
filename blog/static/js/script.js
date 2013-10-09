356fdd6813ce1c8c9e0a7230e3dd37694ef7b674
Bohdan <bohdan.anderson@gmail.com>
2013-10-09 11:11:03 -0400

    added the slider library

---
+++ blog/static/js/script.js
@@ -0,0 +1,129 @@
+var loadingElement = $("#loading")[0];
+var animating = false;
+resizeRows();
+
+$( window ).resize(function() {
+	resizeRows();
+});
+
+function resizeRows(){
+	// console.log("resize Rows");
+	var rows = $(".row");
+	rows.each(function(index){
+		var children = $(rows[index]).children();
+		var height = 0;
+		for(var a = 0; a < children.length; ++a){
+			var nheight = $(children[a]).height();
+			if(nheight > height){
+				height = nheight;
+			}
+			if(children[a].nodeName == "IMG"){
+				children[a].onload = resizeRows;
+			}
+		}
+		rows[index].style.height = height+"px";
+	});
+}
+
+setupnav();
+function setupnav(){
+	// console.log("setup Nav");
+	var links = $(".link-to-page");
+	links.each(function(index){
+		$(links[index]).click(function(){
+			event.preventDefault();
+			if(!animating){
+				animating = true;
+				var linkto = this.getAttribute("href").split("/")
+				clearHighLight();
+				heighLightEl(this);
+				linkto = linkto[linkto.length-1];
+				loadNextPage(linkto);
+			}
+		});
+	});
+}
+
+var slideMenu;
+if($(window).width()>480){
+	slideMenu = $("#nav").pullSlider({inmode:false});
+} else {
+	slideMenu = $("#nav").pullSlider({inmode:true});
+}
+
+$(window).on("resize",function(){
+	if($(window).width()>480){
+		slideMenu.disable();
+	} else {
+		slideMenu.enable();
+	}
+})
+
+
+function loadNextPage(address){
+	// console.log("load next page "+address );
+	loadingElement.style.display = "block";
+	var nextpage = $("#next-page");
+	nextpage.load("/ajax/"+address+"/",function(){
+
+		this.style.left = "0%";
+		this.className = "page animated slideInRight";
+
+		$("#content")[0].className = "page animated slideOutLeft";
+		if (history && history.pushState) {
+			history.pushState("","","/page/"+address+"/");
+		}
+		var newSubnav = $("#subnav-new")[0];
+		var oldSubnav = $("#subnav")[0];
+		newSubnav.style.display = "block";
+		oldSubnav.className = "animated slideOutUp";
+		$("#subnav")[0].parentNode.appendChild(newSubnav);
+		moveButtonDown();
+		newSubnav.className = "animated slideInDown";
+		resizeRows();
+		setTimeout(clearAnimation,1000);
+		setTimeout(resetTheNames,1001);
+	});
+	nextpage.addClass(address);
+}
+
+function moveButtonDown(){
+	var lastB = $(".last-button")[0];
+	lastB.parentNode.appendChild(lastB);
+}
+
+function clearAnimation(){
+	// console.log("clear animation");
+	var elements = $(".animated");
+	elements.removeClass('animated');
+	elements.removeClass('slideInRight');
+}
+
+function clearHighLight(){
+	$(".heighlight").removeClass('heighlight');
+}
+function heighLightEl(element){
+	$(element).addClass('heighlight');
+}
+
+function resetTheNames(){
+	// console.log("reset the names");
+	loadingElement.style.display = "none";
+	var oldcontent = $("#content")[0]
+	var newcontent = $("#next-page")[0]
+	oldcontent.id = "next-page";
+	oldcontent.style.left = "100%";
+	oldcontent.className = "page";
+	newcontent.id = "content";
+
+	newcontent.parentNode.appendChild(newcontent);
+	animating = false;
+	var newSubnav = $("#subnav-new")[0];
+	var oldSubnav = $("#subnav")[0];
+	var parent = oldSubnav.parentNode;
+	parent.removeChild(oldSubnav);
+	newSubnav.id = "subnav";
+	newSubnav.className = "";
+
+	slideMenu.refindHeight();
+}
---
+++ static/js/pullSlider-Jquery.js
@@ -0,0 +1,212 @@
+(function( $ ) {
+
+$.fn.pullSlider = function( options ) {
+	var settings = $.extend({
+		display: false,
+		animationSlideTime: 500,
+		inmode: true,
+		location:"top",
+		//doesn't matter if you change these when you load in the options...
+		element: this,
+		accel: 0,
+		last: 0,
+		height:0,
+		windowHeight:0,
+		toggelbutton: false,
+		tbheight: 0,
+		selected:false
+	}, options );
+
+	switch(settings.location){
+		case "top":
+			settings.toggelbutton = $(this.children()[this.children().length-1]);
+			break;
+		case "bottom":
+			settings.toggelbutton = $(this.children()[0]);
+			break;
+		default:
+			settings.toggelbutton = $(this.children()[this.children().length-1]);
+	}
+	if(settings.inmode){
+		if(!this.hasClass("pull-slider")){
+			this.addClass('pull-slider');
+		}
+		resetHeight();
+		putToPosistion(true);
+	}
+
+	this.disable = function(){
+		if(settings.inmode){
+			settings.element.removeClass('pull-slider');
+			settings.element.css({height:"auto",top:"0px"});
+			settings.inmode = false;
+		}
+	}
+
+	this.enable = function(){
+		if(!settings.inmode){
+			settings.element.addClass('pull-slider');
+			resetHeight();
+			putToPosistion(true);
+			settings.inmode = true;
+		}
+	}
+
+	this.refindHeight = function(){
+		resetHeight();
+		settings.display = false;
+		putToPosistion();
+	}
+
+	$(window).resize(function(){
+		if(settings.inmode){
+			resetHeight();
+			putToPosistion(true);
+		}
+	});
+	//toggle for the desktop and other devices which do not support the touch movements
+	settings.toggelbutton.on("click",function(){
+		if(settings.inmode){
+			if(settings.display){
+				settings.display = false;
+			} else {
+				settings.display = true;
+			}
+			putToPosistion();
+		}
+	});
+	//enable the rest of the touch inputs to work by enabling settings.selected
+	settings.toggelbutton.on("touchstart",function(){
+		if(settings.inmode){
+			settings.toggelbutton.addClass('selected');
+			settings.selected = true;
+			settings.last = settings.element.offset().top;
+		}
+	})
+	//as the user moves over the screen the element follows their touch, by preventing default we do not allow click to be triggered, we also do the math for the acceleration of the menu
+	if (window.addEventListener){
+	window.addEventListener("touchmove",function(event){
+		if(settings.selected){
+			event.preventDefault();
+			var touched = event["targetTouches"]["0"];
+			var thispos = settings.element.offset().top;
+			settings.accel = settings.last - thispos;
+			settings.last = thispos;
+			var tLoc = 0;
+			console.log(event);
+			switch(settings.location){
+				case "top":
+					tLoc = touched["clientY"]-settings.height+(settings.tbheight);
+					break;
+				case "bottom":
+					tLoc = touched["clientY"];
+					break;
+				default:
+					tLoc = touched["clientY"]-settings.height;
+			}
+			console.log(tLoc);
+			if(touched["clientY"]-settings.height < 0 && touched["clientY"]-settings.height > -settings.height){
+				settings.element.css("top",tLoc);
+			}
+		}
+	});
+	}
+	//from the acceleration calced in the touchmove we send the navigation bar to where it needs to end up
+	$(window).on("touchend",function(){
+		if(settings.selected){
+			toggleAnimation(true);
+			switch(settings.location){
+				case "top":
+					if(settings.accel <= 0){
+						settings.display = true;
+					} else {
+						settings.display = false;
+					}
+					break;
+				case "bottom":
+					if(settings.accel >= 0){
+						settings.display = true;
+					} else {
+						settings.display = false;
+					}
+					break;
+			}
+
+			putToPosistion();
+			settings.toggelbutton.removeClass('selected');
+			settings.selected = false;
+		}
+	})
+
+	//set the height of the slider to either the window height if it's greater than it, or to it self, adding the full height style if needed
+	function resetHeight(){
+		settings.windowHeight = $(window).outerHeight();
+		settings.element.height("auto");
+		settings.height = settings.element.height();
+		if(settings.height >= settings.windowHeight){
+			settings.element.height(settings.windowHeight);
+			if(!settings.element.hasClass('slider-window-height')){
+				settings.element.addClass('slider-window-height');
+			}
+		} else {
+			settings.element.height(settings.height);
+			if(settings.element.hasClass('slider-window-height')){
+				settings.element.removeClass('slider-window-height');
+			}
+		}
+		settings.tbheight = settings.toggelbutton.outerHeight(true);
+	}
+	//slide either up or down the element based on the animation
+	function putToPosistion(noscroll){
+		if(settings.inmode){
+			if(settings.display){
+				if(noscroll == undefined || !noscroll){toggleAnimation(true);}
+				switch(settings.location){
+					case "top":
+						settings.element.css("top","0");
+						break;
+					case "bottom":
+						settings.element.css("top",settings.windowHeight-settings.height);
+						break;
+					default:
+						settings.element.css("top","0");
+				}
+			} else {
+				if(noscroll == undefined || !noscroll){toggleAnimation(true)};
+				switch(settings.location){
+					case "top":
+						if(settings.windowHeight <= settings.height){
+							settings.element.css("top",-settings.windowHeight+settings.tbheight);
+						} else {
+							settings.element.css("top",-settings.height+settings.tbheight);
+						}
+						break;
+					case "bottom":
+						settings.element.css("top",settings.windowHeight-settings.tbheight);
+						break;
+					default:
+						settings.element.css("top","0");
+				}
+			}
+			scrollToBottom();
+		}
+	}
+	//toggle the animation class, but if set to true, add it and then remove it after the sliding animation time, if false remove it
+	function toggleAnimation(toggleTo){
+		if(!settings.element.hasClass('slider-animation')||toggleTo){
+			settings.element.addClass('slider-animation');
+			setTimeout(function(){
+				toggleAnimation(false);
+			},settings.animationSlideTime);
+		} else if(settings.element.hasClass('slider-animation')||!toggleTo){
+			settings.element.removeClass('slider-animation');
+		}
+	}
+	//scroll the element to the bottom of the page
+	function scrollToBottom(){
+		settings.element.scrollTop(settings.element[0].scrollHeight);
+	}
+
+	return this
+};
+	}( jQuery ));

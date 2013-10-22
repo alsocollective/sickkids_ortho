6f8eceffdb0dac3f24c195235c5b9ca2a5eb2e30
symonoliver <symon@alsocollective.com>
2013-10-22 18:06:33 -0400

    new analytics

--- blog/static/js/gauniversal.js
+++ blog/static/js/gauniversal.js
@@ -5,3 +5,4 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 // ga('create', 'UA-39447542-2', 'sickkidsorthofellowship.ca', {'cookieDomain': 'none'});
 ga('create', 'UA-39447542-2', 'sickkidsorthofellowship.ca');
+ga('send', 'pageview');

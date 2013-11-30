06eb4f86fb84bcc45068fb766c8962d3acdb472e
symonoliver <symon@alsocollective.com>
2013-11-30 16:12:21 -0500

    analytics and stuff

--- static/js/script.js
+++ static/js/script.js
@@ -225,3 +225,12 @@ function jumpToLocation(){
 	$("#content").scrollTop($("#"+currentHashEl).offset().top + $("#content").scrollTop());
 }
 
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-39447542-1']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
